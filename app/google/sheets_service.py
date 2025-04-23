45d0b94145a5a5fd6b25c5f3b2e1e5c251b5f85d
Vlad <vlabic@mail.ru>
2025-04-23 21:32:45 +0700

    🔐 Авторизация Google через файл

--- app/google/sheets_service.py
+++ app/google/sheets_service.py
@@ -1,25 +1,15 @@
-import os
-import json
 import gspread
 from google.oauth2 import service_account
 from app.config import GOOGLE_SHEET_ID
 
 def get_sheet():
     """
-    Авторизуется через переменную окружения GOOGLE_CREDENTIALS
-    и возвращает доступ к Google Таблице по ID.
+    Авторизация через секретный файл в Render:
+    /etc/secrets/google_credentials.json
     """
-    credentials_raw = os.getenv("GOOGLE_CREDENTIALS")
-    if not credentials_raw:
-        raise ValueError("Переменная GOOGLE_CREDENTIALS не задана")
-    
-    # Преобразуем строку в словарь, заменив \\n на \n
-    credentials_info = json.loads(credentials_raw.replace("\\n", "\n"))
-
     creds = service_account.Credentials.from_service_account_file(
-    "/etc/secrets/google_credentials.json",
-    scopes=["https://www.googleapis.com/auth/spreadsheets"]
-)
-    
+        "/etc/secrets/google_credentials.json",
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
     client = gspread.authorize(creds)
     return client.open_by_key(GOOGLE_SHEET_ID).sheet1
